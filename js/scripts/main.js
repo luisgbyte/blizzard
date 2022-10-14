600b56b6062eea4b3d85781aaeabb835b1df7161
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2022-10-14 18:23:33 -0300

    slide hero - part 02

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,3 +1,15 @@
+var slide_thumbnail = new Swiper(".slide-thumbnail", {
+  slidesPerView: 5,
+  direction: 'vertical',
+  watchSlidesProcess: true
+});
+
 var slide_hero = new Swiper(".slide-principal", {
-  effect: 'fade'
+  effect: 'fade',
+  thumbs: {
+    swiper: slide_thumbnail,
+  },
+  autoplay: {
+    delay: 5000
+  }
 });
