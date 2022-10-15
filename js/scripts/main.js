11fc6e7fb1e1b3ac0f3a65e331829807e81bc2b8
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2022-10-15 19:42:16 -0300

    add section games

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -13,3 +13,23 @@ var slide_hero = new Swiper(".slide-principal", {
     delay: 5000
   }
 });
+
+const allFilters = document.querySelectorAll('.js-nav-games li a');
+const tabPane = document.querySelectorAll('.tab-pane-games');
+
+allFilters.forEach((filter, index) =>
+  filter.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    allFilters.forEach(item => {
+      item.classList.remove('active');
+    });
+
+    tabPane.forEach(item => {
+      item.classList.remove('active');
+    });
+
+    tabPane[index].classList.add('active');
+
+    filter.classList.add('active');
+  }));
